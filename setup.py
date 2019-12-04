96435514b0c899e9f11f2cc2e6bf11f846f0fdfb
AnthonyRonning <anthonyronning@gmail.com>
2019-12-04 16:32:50 -0600

    Adding manifest so tox can run

--- setup.py
+++ setup.py
@@ -8,19 +8,6 @@ with open('requirements.txt') as f:
     install_reqs = f.readlines()
     reqs = [str(ir) for ir in install_reqs]
 
-
-class InstallCommand(Command):
-    description = "Installs MerkleProof2019."
-
-    def run(self):
-        install(reqs)
-
-
-def install(packages):
-    for package in packages:
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
-
-
 setup(
     name='lds-merkle-proof-2019',
     version='0.0.1',
